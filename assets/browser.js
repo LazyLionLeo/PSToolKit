8832348a798dd69f72c6f2f3f1b07c5444a5571f
LazyLionLeo <115797910+LazyLionLeo@users.noreply.github.com>
2020-05-27 21:36:32 +0000

    Forward port zendframework/zf2#3053

--- assets/browser.js
+++ assets/browser.js
@@ -10,4 +10,8 @@ let lastContext;
 let lastPreviewE;
 let lastRunner;
 
-l
+let fileCache = {};
+
+window.addEventListener('message', function({data}) {
+    if (data.command === 'setInput') {
+        update(data.
