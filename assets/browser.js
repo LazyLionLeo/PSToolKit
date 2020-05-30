c7b1ef4c852c3726b56751c5e88ed4ef09e3ab5b
LazyLionLeo <115797910+LazyLionLeo@users.noreply.github.com>
2020-05-30 05:46:49 +0000

    Added a note.

--- assets/browser.js
+++ assets/browser.js
@@ -14,4 +14,7 @@ let fileCache = {};
 
 window.addEventListener('message', function({data}) {
     if (data.command === 'setInput') {
-        update(data.
+        update(data.text, data.path);
+    }
+    else if (data.command === 'scrollTo') {
+        if (lastRunner) lastRunner.scrollTo(data.line, dat
