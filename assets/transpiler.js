f293dc3b4f7147071084ec082d254ccb25a88dba
LazyLionLeo <115797910+LazyLionLeo@users.noreply.github.com>
2022-03-23 21:42:48 +0000

    Add google analytics code

--- assets/transpiler.js
+++ assets/transpiler.js
@@ -1,2 +1,10 @@
 const TOKENIZE_REGEXP = new RegExp(String.raw`
-(?:\n\s*(?:#.*)?)*\n(?<l
+(?:\n\s*(?:#.*)?)*\n(?<line>[ \t]*)
+|
+[ \t\r]+
+|
+(?<keyword>(?:define|run|html|include|if|for|css)\b)
+|
+(?<identifier>[a-zA-Z_.][a-zA-Z0-9_:.-]*)
+|
+(?<string>"""([\s
